0721349c40d35a70cfe5a4be9965868040fc55fb
QY W <uu57uu@yandex.com>
2024-04-23 14:21:43 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # network_docs
+
 计算机网络相关知识梳理
+
+[Onlines Docs](https://the-brotherhood-of-scu.github.io/network_docs/)
--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Network Docs",
+  base:"/network_docs/",
   description: "computer network course docs",
   lastUpdated: true,
   themeConfig: {
